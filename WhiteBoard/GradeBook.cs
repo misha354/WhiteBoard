cd8fe64715f4b3000dae2b7e8caab08922ce3b80
Mike Zhukovskiy <misha354@gmail.com>
2014-12-01 13:27:41 -0500

    summary page

--- WhiteBoard/GradeBook.cs
+++ WhiteBoard/GradeBook.cs
@@ -20,8 +20,32 @@ namespace WhiteBoard
          }
       }
 
-      public int Average()
-      { }
+      //returns the average of the grades in the gradebook
+      public double Average
+      {
+         get
+         {
+            return grades.Average(s => s.NumberGrade);
+         }
+      }
+
+      //returns the maximum grade
+      public int Max
+      {
+         get
+         {
+            return grades.Max(s => s.NumberGrade);
+         }
+      }
+
+      //returns the minimum grade
+      public int Min
+      {
+         get
+         {
+            return grades.Min(s => s.NumberGrade);
+         }
+      }
    
       //Constructor
       public GradeBook(List<Grade> grades)
@@ -29,19 +53,20 @@ namespace WhiteBoard
          this.grades = grades;
       }
 
+      //Methods
+
       // output bar chart displaying grade distribution
       public string OutputBarChart()
       {
          const int NUM_BINS = 11; //Number of bins in the histogram
          string[] BIN_LABELS =  { "00-09", "10-19", "20-29", "30-39", "40-49",
                                    "50-59", "60-69", "70-79", "80-89", "90-99",
-                                   "  100" }; 
+                                   "   100" }; 
 
          int[] bins = new int[NUM_BINS]; //the histogram bins
          
          //the output string
          string histogram = "Grade Distribution:\n";
-
 
          //Build the bin counts
          foreach (Grade grade in grades)
